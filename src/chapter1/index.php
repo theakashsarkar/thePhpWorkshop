7a1993313b31aa88c947cdd4bbf056560e6b93c9
akash <theakashsarkar@gmail.com>
2020-04-06 01:55:31 +0600

    login form html

---
+++ src/chapter1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<h1>Heading Text</h1>
+<p>A paragraph</p>
+<form method="post">    
+    <input type="text" name="domain">   
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
